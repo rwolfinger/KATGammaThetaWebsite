7493d46fa61c4976319dea6ced17389f783f4b45
Rebecca Wolfinger <rwolfing@andrew.cmu.edu>
2013-08-04 17:44:54 -0700

    initial commit

---
+++ WebContent/WEB-INF/js/menucontents.js
@@ -0,0 +1,43 @@
+var anylinkmenu1={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
+anylinkmenu1.items=[
+	["History", "history.html"],
+	["Facts", "facts.html"],
+	["Notable Thetas", "notablethetas.html"],
+	["KA&Theta; Official Site", "http://kappaalphatheta.org"],
+	["Kappa Alpha Theta Foundation", "http://kappaalphathetafoundation.org", "linktarget:_new"] //no comma following last entry!
+]
+
+var anylinkmenu2={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
+anylinkmenu2.items=[
+	["About Gamma Theta", "historyofgammatheta.html"],
+	["Philanthropy & Service", "philanthropy.html"],
+	["Scholarship", "scholarship.html"],
+	["Leadership & Involvement", "leadership.html"],
+	["Sisterhood", "sisterhood.html"]
+]
+
+var anylinkmenu3={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
+anylinkmenu3.items=[
+	["Greek Sing", "greeksing.html"],
+	["Booth", "booth.html"],
+	["Formal", "formal.html"],
+	["Crush Parties", "crushparties.html"] //no comma following last entry!
+]
+
+var anylinkmenu4={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
+anylinkmenu4.items=[
+	["Officers", "officers.html"],
+	["Fall 2009", "fall09.html"],
+	["Fall 2010 & Spring 2011", "fall10.html"],
+	["Fall 2011", "fall11.html"],
+	["Fall 2012", "fall12.html"]
+]
+
+var anylinkmenu5={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
+anylinkmenu5.items=[
+	["Why Go Greek?", "whygogreek.html"],
+	["Recruitment 2012", "recruitment.html"]
+]
+
+var anylinkmenu6={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
+anylinkmenu6.items=[]
