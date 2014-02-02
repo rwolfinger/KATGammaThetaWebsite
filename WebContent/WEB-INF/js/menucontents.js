2ec8b030494afa190e01010c35aee735cefb506e
Rebecca Wolfinger <rwolfing@andrew.cmu.edu>
2014-02-02 16:09:59 -0500

    2/2 updated: fall13 and other changes
    fixed pictures/ graduation dates/ etc.

--- WebContent/WEB-INF/js/menucontents.js
+++ WebContent/WEB-INF/js/menucontents.js
@@ -19,7 +19,8 @@ anylinkmenu3.items=[
 	["Officers", "officers.html"],
 	["Fall 2010", "fall10.html"],
 	["Fall 2011", "fall11.html"],
-	["Fall 2012", "fall12.html"]
+	["Fall 2012", "fall12.html"],
+	["Fall 2013", "fall13.html"]
 ]
 
 var anylinkmenu4={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
